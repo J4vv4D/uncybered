541b49e7776907b8b4a8fe8eec4dfdae09b84c39
pxlphile <pxlphile@example.com>
2018-02-08 14:17:08 +0100

    Extracts methods and rearranges code that is directly run
    Some code which does not define a function is spread through out the
    content.js file. This commit seeks to consolidate this code to a lower
    number, like directly at the file head for constant declaration, or the
    file end.
    
    The main routine on top-level was extracted into the function
    "runUncybered" which itself still showed a high number of encapsulation
    of fors and ifs. That part that takes over the heavy lifting was
    extracted into its own function for better overview.
    
    One last inline function definition was replaced by a fat arrow function
    
    Also some JSDoc comments have been placed. Further are to come.

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
-const elements = document.getElementsByTagName('*');
+const DOM_NODE_TYPE_TEXT = 3;
+const debugToConsole = true;
 
+/**
+ * Contains a map of uncybered word lists to a single cybered word
+ */
 const sourceWordsToTargetWords = [
     [['machine learning', 'ml'], 'magic'],
     [['artificial intelligence', 'ai'], 'witchcraft'],
@@ -16,6 +20,11 @@ const sourceWordsToTargetWords = [
     [['forrester'], 'generic analyst firm'],
     [['451 research'], 'generic analyst firm'],
 ];
+
+/**
+ * Contains an array of regexes which help to replace cybered words.
+ */
+let sourceRegexToTargetWords;
 
 function makeRegex(sourceWords) {
     return new RegExp('\\b' + sourceWords.join('\\b|\\b') + '\\b', 'gi');
@@ -34,17 +43,13 @@ function toUpperCase(string) {
 };
 
 function makeRegexToTargetWords(sourceWordsToTargetWords, modifyWords) {
-    return sourceWordsToTargetWords.map(function (sourceAndTarget) {
+    return sourceWordsToTargetWords.map(sourceAndTarget => {
         let [source, target] = sourceAndTarget;
         source = source.map(modifyWords);
         target = modifyWords(target);
         return [makeRegex(source), target];
     });
 };
-
-const sourceRegexToTargetWords = makeRegexToTargetWords(sourceWordsToTargetWords, identity)
-    .concat(makeRegexToTargetWords(sourceWordsToTargetWords, capitalizeFirstLetter))
-    .concat(makeRegexToTargetWords(sourceWordsToTargetWords, toUpperCase));
 
 function replaceTextWithRegexes(text, sourceRegexToTargetWords) {
     for (let k = 0; k < sourceRegexToTargetWords.length; k++) {
@@ -54,20 +59,68 @@ function replaceTextWithRegexes(text, sourceRegexToTargetWords) {
     return text;
 };
 
-for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
+/**
+ * Prints text to the console when a debugging flag is true.
+ * @param {string} textToLog - the text that should be logged to the console
+ *
+ * @return {void}
+ */
+function debuglog(textToLog) {
+    if (debugToConsole) {
+        console.log(textToLog);
+    }
+}
 
+/**
+ * replace the given element's nonsensical text with a more understandable text from the mapping
+ * @param {HTMLElement} element - the element which contains text that will potentially be replaced
+ *
+ * @return {void}
+ */
+function uncyberElementIfNecessary(element) {
     for (let j = 0; j < element.childNodes.length; j++) {
         const node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
+        if (node.nodeType === DOM_NODE_TYPE_TEXT) {
             const text = node.nodeValue;
-            replacedText = replaceTextWithRegexes(text, sourceRegexToTargetWords);
+            let replacedText = replaceTextWithRegexes(text, sourceRegexToTargetWords);
 
             if (replacedText !== text) {
-                console.log('replaced');
+                debuglog('replaced ' + text + ' with ' + replacedText);
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
 }
+
+/**
+ * Initializes the array which holds the regex to replace cybered target words. This should be done upfront because of
+ * performance implications of regexp compilation.
+ *
+ * Side effect: the global variable <tt>sourceRegexToTargetWords</tt> is set.
+ *
+ * @return {void}
+ */
+function initializeRegexToTargetWords() {
+    sourceRegexToTargetWords = makeRegexToTargetWords(sourceWordsToTargetWords, identity)
+        .concat(makeRegexToTargetWords(sourceWordsToTargetWords, capitalizeFirstLetter))
+        .concat(makeRegexToTargetWords(sourceWordsToTargetWords, toUpperCase));
+}
+
+/**
+ * This is the main routine which starts the uncybering, that is replacing nonsensical target words with sane ones. It
+ * iterates through all elements in the DOM tree searches and replaces them with appropriate mappings.
+ *
+ * @return {void}
+ */
+function runUncybered() {
+    initializeRegexToTargetWords();
+
+    let elements = document.getElementsByTagName('*');
+    elements = Array.prototype.slice.call(elements);
+    elements.forEach(element => {
+        uncyberElementIfNecessary(element);
+    });
+}
+
+runUncybered();
